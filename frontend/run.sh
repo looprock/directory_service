bd61b39589b913ed2c0467159359993933a9d82a
Douglas Land <dland@homestoryrewards.com>
2025-03-05 08:24:51 -0600

    improve description

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 . .env
 
-docker build -t directory-service-fe .
+# docker build -t directory-service-fe .
+docker pull ghcr.io/looprock/directory_service_frontend:dev-latest
 
-docker run \
+docker run -d \
 -p 3000:3000 \
 -e NEXTAUTH_URL=${NEXTAUTH_URL} \
 -e NEXTAUTH_SECRET=${NEXTAUTH_SECRET} \
@@ -11,4 +12,4 @@ docker run \
 -e ADMIN_API_KEY=${ADMIN_API_KEY} \
 -e GOOGLE_ID=${GOOGLE_ID} \
 -e GOOGLE_SECRET=${GOOGLE_SECRET} \
-directory-service-fe
+ghcr.io/looprock/directory_service_frontend:dev-latest
